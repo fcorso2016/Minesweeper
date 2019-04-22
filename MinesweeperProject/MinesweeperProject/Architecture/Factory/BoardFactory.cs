6a17d8e571f2e915541b4b5ded34e529429e842a
fcorso2016 <fcorso2016@gmail.com>
2019-04-22 09:44:57 -0400

    Fixed board representation system

--- MinesweeperProject/MinesweeperProject/Architecture/Factory/BoardFactory.cs
+++ MinesweeperProject/MinesweeperProject/Architecture/Factory/BoardFactory.cs
@@ -4,13 +4,18 @@ namespace MinesweeperProject.Architecture.Factory {
     public class BoardFactory : IBoardFactory {
         public Square MakeSquare(char sq, int x, int y) {
             switch (sq) {
-                case ' ':
+                case '0':
                     return new EmptySquare(x, y);
                 case 'X':
                     return new Mine(x, y);
                 default:
-                    NumberSquare ns = new NumberSquare(x, y, sq - '0');
-                    return ns;
+                    if (sq >= '1' && sq <= '9') {
+                        NumberSquare ns = new NumberSquare(x, y, sq - '0');
+                        return ns;
+                    }
+
+                    // If not a number, then it doesn't know what to do with the value
+                    return null;
             }
         }
     }
